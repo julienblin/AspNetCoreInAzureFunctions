26fc613ed27e970201802aeda09603ca553fd3fd
Julien Blin <julien.blin@gmail.com>
2019-03-31 10:48:29 -0400

    Minor refactoring for AzureFunctionsFeatures.

--- AspNetCoreInAzureFunctions/AzureFunctionsFeatures.cs
+++ AspNetCoreInAzureFunctions/AzureFunctionsFeatures.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net;
@@ -20,9 +18,10 @@ namespace AspNetCoreInAzureFunctions
     [SuppressMessage("Design", "CA1001:Types that own disposable fields should be disposable", Justification = "HttpResponseMessage will be disposed by the functions runtime.")]
     [SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "Collection suffix conflict with standard ASP.Net core naming conventions.")]
     public sealed class AzureFunctionsFeatures
-        : IFeatureCollection, IHttpRequestFeature, IHttpResponseFeature, IHttpResponseMessageFeature, IAzureFunctionExecutionContextFeature, IAzureFunctionLoggerFeature
+        : FeatureCollection,
+        IHttpRequestFeature, IHttpResponseFeature, IHttpRequestIdentifierFeature,
+        IHttpResponseMessageFeature, IAzureFunctionExecutionContextFeature, IAzureFunctionLoggerFeature
     {
-        private readonly IDictionary<Type, object> _features = new Dictionary<Type, object>();
         private readonly HttpRequest _request;
         private readonly ILogger _logger;
         private readonly ExecutionContext _executionContext;
@@ -38,29 +37,24 @@ namespace AspNetCoreInAzureFunctions
         {
             _request = request ?? throw new ArgumentNullException(nameof(request));
             _responseMessage = new HttpResponseMessage();
-
-            _features[typeof(IHttpRequestFeature)] = this;
-            _features[typeof(IHttpResponseFeature)] = this;
-            _features[typeof(IHttpResponseMessageFeature)] = this;
+            Set<IHttpRequestFeature>(this);
+            Set<IHttpResponseFeature>(this);
+            Set<IHttpResponseMessageFeature>(this);
 
             if (executionContext != null)
             {
                 _executionContext = executionContext;
-                _features[typeof(IAzureFunctionExecutionContextFeature)] = this;
+                Set<IAzureFunctionExecutionContextFeature>(this);
+                Set<IHttpRequestIdentifierFeature>(this);
+                ((IHttpRequestIdentifierFeature)this).TraceIdentifier = _executionContext.InvocationId.ToString();
             }
 
             if (logger != null)
             {
                 _logger = logger;
-                _features[typeof(IAzureFunctionLoggerFeature)] = this;
+                Set<IAzureFunctionLoggerFeature>(this);
             }
         }
-
-        /// <inheritdoc />
-        bool IFeatureCollection.IsReadOnly => false;
-
-        /// <inheritdoc />
-        int IFeatureCollection.Revision => 0;
 
         /// <inheritdoc />
         string IHttpRequestFeature.Protocol { get => _request.Protocol; set => _request.Protocol = value; }
@@ -122,38 +116,7 @@ namespace AspNetCoreInAzureFunctions
         ILogger IAzureFunctionLoggerFeature.Logger => _logger;
 
         /// <inheritdoc />
-        object IFeatureCollection.this[Type key]
-        {
-            get
-            {
-                if (_features.TryGetValue(key, out var feature))
-                {
-                    return feature;
-                }
-
-                return null;
-            }
-
-            set
-            {
-                _features[key] = value;
-            }
-        }
-
-        /// <inheritdoc />
-        TFeature IFeatureCollection.Get<TFeature>()
-        {
-            return (TFeature)((IFeatureCollection)this)[typeof(TFeature)];
-        }
-
-        /// <inheritdoc />
-        IEnumerator<KeyValuePair<Type, object>> IEnumerable<KeyValuePair<Type, object>>.GetEnumerator() => _features.GetEnumerator();
-
-        /// <inheritdoc />
-        IEnumerator IEnumerable.GetEnumerator() => _features.GetEnumerator();
-
-        /// <inheritdoc />
-        void IFeatureCollection.Set<TFeature>(TFeature instance) => _features[typeof(TFeature)] = instance;
+        string IHttpRequestIdentifierFeature.TraceIdentifier { get; set; }
 
         /// <inheritdoc />
         void IHttpResponseFeature.OnStarting(Func<object, Task> callback, object state)
@@ -171,7 +134,7 @@ namespace AspNetCoreInAzureFunctions
             HasStarted = true;
             var httpResponseFeature = (IHttpResponseFeature)this;
 
-            if (httpResponseFeature.Body != null)
+            if (httpResponseFeature.Body != null && httpResponseFeature.Body.Length > 0)
             {
                 var contentLength = httpResponseFeature.Body.Length;
                 httpResponseFeature.Body.Seek(0, SeekOrigin.Begin);
@@ -180,8 +143,10 @@ namespace AspNetCoreInAzureFunctions
 
             foreach (var header in httpResponseFeature.Headers)
             {
-                _responseMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
-                _responseMessage.Content?.Headers?.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                if (!_responseMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray()))
+                {
+                    _responseMessage.Content?.Headers?.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                }
             }
 
             return _responseMessage;

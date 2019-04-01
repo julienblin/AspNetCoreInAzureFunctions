2118f29f5d29041ca16060365c950707f62e8182
Julien Blin <julien.blin@gmail.com>
2019-04-01 16:50:28 -0400

    Added more tests.

--- AspNetCoreInAzureFunctions.Tests/Api/ApiController.cs
+++ AspNetCoreInAzureFunctions.Tests/Api/ApiController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AspNetCoreInAzureFunctions.Features;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
 
 namespace AspNetCoreInAzureFunctions.Tests.Api
 {
@@ -7,12 +9,29 @@ namespace AspNetCoreInAzureFunctions.Tests.Api
     public class ApiController : ControllerBase
     {
         public const string ModelUri = "";
+        public const string ExecutionContextUri = "executionContext";
+        public const string ClaimsPrincipalUri = "claimsPrincipal";
 
         [HttpGet(ModelUri)]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApiModel))]
         public IActionResult GetModel()
         {
             return Ok(new ApiModel { Name = "AspNetCoreInAzureFunctions" });
+        }
+
+        [HttpPost(ExecutionContextUri)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ExecutionContext))]
+        public IActionResult PostExecutionContext()
+        {
+            var feature = HttpContext.Features.Get<IAzureFunctionExecutionContextFeature>();
+            return Ok(feature.ExecutionContext);
+        }
+
+        [HttpPut(ClaimsPrincipalUri)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public IActionResult PutClaimsPrincipal()
+        {
+            return Ok(HttpContext.User.Identity.Name);
         }
     }
 }
--- AspNetCoreInAzureFunctions.Tests/AzureFunctionsServerTests.cs
+++ AspNetCoreInAzureFunctions.Tests/AzureFunctionsServerTests.cs
@@ -1,10 +1,15 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AspNetCoreInAzureFunctions.Tests.Api;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace AspNetCoreInAzureFunctions.Tests
@@ -61,6 +66,79 @@ namespace AspNetCoreInAzureFunctions.Tests
             var response = await _fixture.Server.ProcessRequestAsync(request);
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task ItShouldGetExecutionContext()
+        {
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Method = "POST",
+                Path = $"/{ApiController.ExecutionContextUri}",
+            };
+
+            var executionContext = new ExecutionContext();
+
+            var response = await _fixture.Server.ProcessRequestAsync(request, executionContext: executionContext);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var model = await response.Content.ReadAsAsync<ExecutionContext>();
+            model.Should().BeEquivalentTo(executionContext);
+        }
+
+        [Fact]
+        public async Task ItShouldLog()
+        {
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Method = "GET",
+                Path = $"/{ApiController.ModelUri}",
+            };
+
+            var logger = new TestLogger();
+
+            var response = await _fixture.Server.ProcessRequestAsync(request, logger: logger);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            logger.Messages.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task ItShouldHandleClaimsPrincipal()
+        {
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Method = "PUT",
+                Path = $"/{ApiController.ClaimsPrincipalUri}",
+            };
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "John Doe") }));
+
+            var response = await _fixture.Server.ProcessRequestAsync(request, claimsPrincipal: claimsPrincipal);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var name = await response.Content.ReadAsStringAsync();
+            name.Should().Be(claimsPrincipal.Identity.Name);
+        }
+
+        private class TestLogger : ILogger
+        {
+            public IList<object> Messages { get; } = new List<object>();
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                Messages.Add(state);
+            }
         }
     }
 }
